0d49dfe0b9aad24f75f676a6f9ea1d58a9e765b2
Matheus Felipe <matheus.reirasi@gmail.com>
2022-08-29 10:28:53 -0300

    added components

---
+++ react-typescript/aula-1/src/components/Botao.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Botao extends React.Component {
+    render(): React.ReactNode { //Não tem React.ReactNode no vídeo da aula, somente o render()
+        return(
+            <button>
+                Botão
+            </button>
+        )
+    }
+}
+
+export default Botao
