0bd9379348c83cdb31f963b235adb0ee28cecfd0
Matheus Felipe <matheus.reirasi@gmail.com>
2022-09-03 16:22:12 -0300

    input and secret word is lower case and inserted list comprehensions option

--- python-iniciando/jogos/forca.py
+++ python-iniciando/jogos/forca.py
@@ -5,31 +5,39 @@ def jogar(): ## em javascript, isso seria uma function
     print("Bem Vindo ao jogo da forca")
     print("************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "arroz".lower()
     palavra = ["_"]*len(palavra_secreta)
+    #palavra = ["_" for letra in palavra_secreta] isso é list comprehensions
     enforcou = False
     acertou = False
     soma_de_palavras = 0
+    tentativas = 0
 
     print(palavra)
 
-    while (not enforcou and not acertou):
+    while (not enforcou and not acertou): #enquanto enforcou e acertou continua falso
 
         chute = input("Tente uma letra: ")
-        chute = chute.strip() #Como nenhum parâmetro foi passado, removerá tudo que contenha um espaço, como por exemplo, "  arroz "
-        index = 0
+        chute = chute.strip().lower() #Como nenhum parâmetro foi passado, removerá tudo que contenha um espaço, como por exemplo, "  arroz "
 
-        for letra in palavra_secreta:
-            if (chute.lower() == letra.lower()):
-                palavra[index] = chute
-                soma_de_palavras += 1
-            index += 1
 
-        if soma_de_palavras == len(palavra_secreta):
-            print("\nVoce acertou!")
-            acertou = True
-            
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    palavra[index] = chute
+                    soma_de_palavras += 1
+                index += 1
+        else: ## caso de tentativas erradas
+            tentativas += 1
+
+
         print(palavra)
+
+        if (tentativas == 6):
+            enforcou = True
+            print("Você foi enforcado.")
+            
 
 if (__name__ == "__main__"):
     jogar()
