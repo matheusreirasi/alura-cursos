9cde2e80079a1a92755f49a3d27099f4fb74c5ba
Matheus Felipe <matheus.reirasi@gmail.com>
2022-09-10 11:09:46 -0300

    random number

---
+++ linguagem-c/conceitos/numero-aleatorio.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int segundos = time(0);
+    srand(segundos);
+
+    int a = rand();
+    int b = rand();
+
+    printf("%d \n%d \n",a ,b);
+}
+
+
+/*
+* O computador não consegue realmente gerar números aleatórios para o usuário, são números pseudo-aleatórios. Ao utilizar a função rand() para tentar gerar um número aleatório, a função estará na mesma raiz (seed) sempre que executada, e somente assim não é capaz de gerar um número aleatório, pois a função matemática é sempre a mesma.
+* Então para realmente fazer a função retornar um valor aleatório é necessário alterar a raiz (seed). Para isso, um dos meios utilizados é utilizando a função time() e colocando esse time dentro da função srand().
+* A função time(0) retorna o número de segundos passados desde 1 de janeiro de 1970 até o momento atual. Essa data também é conhecida na programação como EPOCH.
+*/
