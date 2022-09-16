86540ce177e13537a10578869cdd748a8c6fda2c
Matheus Felipe <matheus.reirasi@gmail.com>
2022-09-15 23:58:05 -0300

    ascii art

--- linguagem-c/jogos/adivinhacao.c
+++ linguagem-c/jogos/adivinhacao.c
@@ -3,12 +3,17 @@
 #include <time.h>
 
 
-#define TENTATIVAS_TOTAIS 10 //define é utilizado quando uma variável é utilizada somente uma vez, sendo assim chamada de constantes.
 
 int main() {
-    printf("************************************\n");
-    printf("* Bem Vindo ao jogo de adivinhação *\n");
-    printf("************************************\n");
+    printf("\n\n");
+    printf("          P  /_\\  P                              \n");
+    printf("         /_\\_|_|_/_\\                            \n");
+    printf("     n_n | ||. .|| | n_n         Bem vindo ao     \n");
+    printf("     |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação! \n");
+    printf("    |\" \"  |  |_|  |\"  \" |                     \n");
+    printf("    |_____| ' _ ' |_____|                         \n");
+    printf("          \\__|_|__/                              \n");
+    printf("\n\n");
 
     int segundos = time(0);
     srand(segundos);
@@ -21,12 +26,32 @@ int main() {
     int jogando = 0;
     float pontos = 1000;
 
-    while(jogando == 0){
+    int tentativasTotais;
+    int nivel;
+    printf("Qual o nível de dificuldade?\n");
+    printf("(1)Fácil (2)Médio (3)Difícil\n");
+    scanf("%d", &nivel);
 
-        printf("Tentativa %d de %d\n", tentativas, TENTATIVAS_TOTAIS);
+    switch (nivel) {
+        case 1:
+            tentativasTotais = 20;
+            break;
+        
+        case 2:
+            tentativasTotais = 12;
+            break;
+
+        default:
+            tentativasTotais = 6;
+            break;
+    }
+
+
+    for (int i = 1; i <= tentativasTotais; i++){
+
+        printf("Tentativa %d de %d\n", tentativas, tentativasTotais);
         printf("Qual é o seu chute?\n");
         scanf("%u", &chute);
-        printf("Seu chute foi %u \n", chute);
 
         if(chute < 1 || chute > 100){
             printf("Você deve digitar um número entre 1 e 100\n");
@@ -35,8 +60,24 @@ int main() {
         }
 
         if ( chute == numeroSecreto) {
-            printf("Parabéns! Você acertou.\n");
-
+            printf("\n");
+            printf("             OOOOOOOOOOO               \n");
+            printf("         OOOOOOOOOOOOOOOOOOO           \n");
+            printf("      OOOOOO  OOOOOOOOO  OOOOOO        \n");
+            printf("    OOOOOO      OOOOO      OOOOOO      \n");
+            printf("  OOOOOOOO  #   OOOOO  #   OOOOOOOO    \n");
+            printf(" OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   \n");
+            printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+            printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+            printf("OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  \n");
+            printf(" OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   \n");
+            printf("  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    \n");
+            printf("    OOOOO   OOOOOOOOOOOOOOO   OOOO     \n");
+            printf("      OOOOOO   OOOOOOOOO   OOOOOO      \n");
+            printf("         OOOOOO         OOOOOO         \n");
+            printf("             OOOOOOOOOOOO              \n");
+            printf("\nParabéns! Você acertou!\n");
+            printf("Você fez %.2f pontos. Até a próxima!\n\n", pontos);
             break;
             //jogando = 1 (Outro método de finalizar o while)
         }
@@ -54,8 +95,7 @@ int main() {
         pontos -= pontosPerdidos;
     }
     printf("Fim de jogo\n");
-    printf("Você acertou em %d tentativas.\n", tentativas);
-    printf("Total de pontos: %.2f \n", pontos);
+    
 
     return 0;
 }
@@ -63,4 +103,5 @@ int main() {
 
 /*
 * Para usar a função abs() é necessário incluir a biblioteca stdlib.h
+* O nome dessas figuras que são impressas no terminal são ascii art
 */
