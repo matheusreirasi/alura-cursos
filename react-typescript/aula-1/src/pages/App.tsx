0074fe8f52a1ea276bcbc6204d6a4c0424760fa6
Matheus Felipe <matheus.reirasi@gmail.com>
2022-12-26 12:12:12 -0300

    tasks

--- react-typescript/aula-1/src/pages/App.tsx
+++ react-typescript/aula-1/src/pages/App.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Formulario from '../components/Formulario/Formulario';
 import Lista from '../components/Lista/Lista';
 import Cronometro from '../components/Cronometro/Cronometro';
 import style from "./App.module.scss"
+import { ITarefa } from '../types/tarefas';
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([])
+  const [selecionado, setSelecionado] = useState<ITarefa>()
+
+  const selecionaTarefa = (tarefaSelecionada: ITarefa)=>{
+    setSelecionado(tarefaSelecionada)
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
+
   return (
     <div className={style.AppStyle}>
 
-      <Formulario />
-      <Lista />
-      <Cronometro />
+      <Formulario setTarefas={setTarefas}/>
+      <Lista tarefas={tarefas} selecionaTarefa={selecionaTarefa}/>
+      <Cronometro selecionado={selecionado}/>
     </div>
   );
 }
 
 export default App;
+
+//Teve que ser passado tarefas para cá porque quando se utiliza props somente é possível um componente pai passar um props para um componente filho. Não é possível passar props de um componente filho para um componente pai. Como era necessário passar os valores do formulário para lista, foi passado tarefas e setTarefas para cá. Agora essa conversa ocorre porque foi criado parâmetros(props) para cada componente que eu criei e através desses props que um valor contido em um componente aparece em outro componente
