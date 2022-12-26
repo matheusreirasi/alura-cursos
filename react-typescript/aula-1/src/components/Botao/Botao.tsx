edd9b7aacca2cd7405297e88c0d8799ca5c57a39
Matheus Felipe <matheus.reirasi@gmail.com>
2022-12-26 12:13:44 -0300

    button

--- react-typescript/aula-1/src/components/Botao/Botao.tsx
+++ react-typescript/aula-1/src/components/Botao/Botao.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import style from "./Botao.module.scss"
 
-class Botao extends React.Component <{text: string}> {
+class Botao extends React.Component <{text: string, type?: "button" | "submit" | "reset" | undefined}> {
     render(): React.ReactNode { //Não tem React.ReactNode no vídeo da aula, somente o render()
+        const { type = "button"} = this.props
         return(
-            <button className={style.botao}>
+            <button type = {type} className={style.botao}>
                 {this.props.text}
             </button>
         )
@@ -14,4 +15,7 @@ class Botao extends React.Component <{text: string}> {
 export default Botao
 
 //Props funciona como atributos. Eu tenho que estabelecer as props na classe que lida com as interações, nesse caso é o botão.
+
 //Primeiro eu especifico os props que farão parte da classe (nesse caso são as props do botão) e depois digo onde essa props irá aparecer(dentro da tag button). Desse jeito, sempre que eu usar esse componente em outra parte do meu código eu só preciso colocar "text='Adicionar'" no local do botão onde ele será inserido
+
+//Se nenhum tipo for passado para o botão então ele será to tipo button
