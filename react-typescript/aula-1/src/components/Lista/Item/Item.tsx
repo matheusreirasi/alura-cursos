33dbd86561722a1423a74b15c3e77a62d717aae8
Matheus Felipe <matheus.reirasi@gmail.com>
2022-12-27 11:53:18 -0300

    coloquei o estilo de tarefa finalizada, tarefa fica verde e aparece um logo de finalizado

--- react-typescript/aula-1/src/components/Lista/Item/Item.tsx
+++ react-typescript/aula-1/src/components/Lista/Item/Item.tsx
@@ -7,19 +7,29 @@ interface Props extends ITarefa {
 
 const Item = ({tarefa, tempo, selecionado, completado, id, selecionaTarefa}:Props)=> {
     return (
-        <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`} onClick={()=> selecionaTarefa({
-            tarefa,
-            tempo,
-            selecionado,
-            completado,
-            id
-        })}>
+        <li
+            className={`
+                ${style.item} 
+                ${selecionado ? style.itemSelecionado : ''} 
+                ${completado ? style.itemCompletado : ''}
+            `}
+            onClick={()=> !completado && selecionaTarefa({
+                tarefa,
+                tempo,
+                selecionado,
+                completado,
+                id
+            })
+        }>
             <h3>
                 {tarefa}
             </h3>
             <span>
                 {tempo}
             </span>
+            {completado &&
+                <span className={style.concluido} aria-label="tarefa completada">
+                </span>}
         </li>
     )
 }
