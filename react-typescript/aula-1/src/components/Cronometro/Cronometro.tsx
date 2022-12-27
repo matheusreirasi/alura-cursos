fd44e4432b8f92f8b6cdc74de04f2f5f6137ec59
Matheus Felipe <matheus.reirasi@gmail.com>
2022-12-27 11:50:48 -0300

    função para regredir o tempo

--- react-typescript/aula-1/src/components/Cronometro/Cronometro.tsx
+++ react-typescript/aula-1/src/components/Cronometro/Cronometro.tsx
@@ -5,15 +5,28 @@ import { ITarefa } from '../../types/tarefas'
 import tempoParaSegundos from '../../utils/time'
 
 interface Props {
-  selecionado: ITarefa | undefined
+  selecionado: ITarefa | undefined,
+  finalizarTarefa: ()=> void
 }
 
-const Cronometro = ({selecionado}: Props) => {
+const Cronometro = ({selecionado, finalizarTarefa}: Props) => {
   const [tempo, setTempo] = useState<number>()
 
   useEffect(() => {
     setTempo(tempoParaSegundos(String(selecionado?.tempo)))
   }, [selecionado])
+
+
+  const regressiva = (contador: number = 0)=>{//coloco 0 para evitar o undefined 
+    setTimeout(() => {
+      if (contador > 0) {
+        setTempo(contador - 1)
+        return regressiva(contador - 1)
+      }
+      finalizarTarefa()
+    }, 1000);
+  }
+  
 
   const minutos = Math.floor(Number(tempo) / 60)
   const segundos = Number(tempo) % 60
@@ -25,7 +38,6 @@ const Cronometro = ({selecionado}: Props) => {
         <p  className={style.titulo}>
             Escolha um card e inicie o cronômetro
         </p>
-        Tempo: {tempo}
         <div className={style.relogioWrapper}>
             <span className={style.relogioNumero}>{minutoDezena}</span>
             <span className={style.relogioNumero}>{minutoUnidade}</span>
@@ -34,7 +46,8 @@ const Cronometro = ({selecionado}: Props) => {
             <span className={style.relogioNumero}>{segundoUnidade}</span>
         </div>
         <Botao 
-            text='Começar!'
+          text='Começar!'
+          onClick={()=>regressiva(tempo)}
         />
     </div>
   )
@@ -48,3 +61,5 @@ export default Cronometro
 //Type 'string' can only be iterated through when using the '--downlevelIteration' flag or with a '--target' of 'es2015' or higher. Quando eu desestruturei String(minutos) para tratar minutos como um array de char, o typescript retornou esse erro.
 
 //A função padStart serviu para eu renderizar a string "0" quando eu não tiver a cadeia de strings completa com todos os valores necessários. Isso ocorre quando eu coloco no tempo de estudos 1 hora por exemplo. Quando isso acontece é mostrado na tela somente 1 e após ele e antes do dois pontos fica um nada, simplesmente vazio.
+
+//Esse componente Botao que eu criei não aceita esse parâmetro onClick que geralmente é padrão, por isso tive que criar dentro da função do componente
