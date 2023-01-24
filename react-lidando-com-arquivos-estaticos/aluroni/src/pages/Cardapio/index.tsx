ce8dafb77e3bca619a42d47d9dbe5f19d693c6d0
matheusreirasi <matheus.reirasi@gmail.com>
2023-01-23 23:46:18 -0300

    insert order and filter

--- react-lidando-com-arquivos-estaticos/aluroni/src/pages/Cardapio/index.tsx
+++ react-lidando-com-arquivos-estaticos/aluroni/src/pages/Cardapio/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import { useState } from 'react'
+
 import styles from "./Cardapio.module.scss"
 import {ReactComponent as Logo} from "assets/logo.svg"
 import { Buscador } from './Buscador'
-import { useState } from 'react'
 import { Filtro } from './Filtro'
+import { Ordenador } from './Ordenador'
+import { Itens } from './Itens'
 
 export const Cardapio = () => {
   const [busca, setBusca] = useState("")
+  const [filtro, setFiltro] = useState<number | null>(null)
+  const [ordenador, setOrdenador] = useState("")
 
   return (
     <main>
@@ -29,8 +33,10 @@ export const Cardapio = () => {
           setBusca={setBusca}
         />
         <div className={styles.cardapio__filtros}>
-          <Filtro />
+          <Filtro filtro={filtro} setFiltro={setFiltro}/>
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
         </div>
+        <Itens busca={busca} filtro={filtro} ordenador={ordenador}/>
       </section>
 
     </main>
