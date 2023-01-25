827cc81b885769fcc9cc36c3097381cd2c862f3c
matheusreirasi <matheus.reirasi@gmail.com>
2023-01-25 20:34:12 -0300

    integração entre o servidor e o banco de dados

--- api-com-express-e-mongodb/src/app.js
+++ api-com-express-e-mongodb/src/app.js
@@ -1,6 +1,8 @@
 import express from "express"
 import db from "./config/db.js"
 import livros from "./models/Livro.js"
+import routes from "./routes/index.js"
+
 
 db.on("error", console.log.bind(console, "Erro de conexão"))
 db.once("open", () => {
@@ -11,18 +13,9 @@ const app = express()
 
 app.use(express.json())//sem isso ñ consigo passar para o post um objeto json. Se eu tentar irá receber null
 
+routes(app)
 
 
-
-app.get("/", (req, res) => {
-    res.status(200).send("Curso de Node")
-})
-
-app.get("/livros", (req, res) => {
-    livros.find((err, livros) =>{
-        res.status(200).json(livros)
-    })
-})
 
 app.get("/livros/:id", (req, res) => {
     let index = buscaLivro(req.params.id)
