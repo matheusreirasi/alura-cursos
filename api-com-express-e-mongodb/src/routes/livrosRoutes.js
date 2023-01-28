282904f97a313152b0bee9c9f38c217537eda845
matheusreirasi <matheus.reirasi@gmail.com>
2023-01-28 00:35:22 -0300

    verbos HTML contendo as funções que serão utilizadas

--- api-com-express-e-mongodb/src/routes/livrosRoutes.js
+++ api-com-express-e-mongodb/src/routes/livrosRoutes.js
@@ -4,6 +4,11 @@ import LivroController from "../controllers/livrosController.js"
 const router = express.Router()
 
 router.get("/livros", LivroController.listarLivros)
+router.get('/livros/busca', LivroController.listarLivroEditora)
+router.get("/livros/:id", LivroController.listarLivroId)
+router.post("/livros", LivroController.cadastrarLivro)
+router.put("/livros/:id", LivroController.atualizarLivro)
+router.delete("/livros/:id", LivroController.removerLivro)
 
 
 export default router
