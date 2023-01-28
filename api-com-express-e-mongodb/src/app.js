0945dd3a5f3b6bd9bc7fa9782be51aaafdbdbb4b
matheusreirasi <matheus.reirasi@gmail.com>
2023-01-28 00:30:37 -0300

    removi todos os comandos HTTP daqui

--- api-com-express-e-mongodb/src/app.js
+++ api-com-express-e-mongodb/src/app.js
@@ -1,6 +1,5 @@
 import express from "express"
 import db from "./config/db.js"
-import livros from "./models/Livro.js"
 import routes from "./routes/index.js"
 
 
@@ -10,40 +9,8 @@ db.once("open", () => {
 })
 
 const app = express()
-
 app.use(express.json())//sem isso ñ consigo passar para o post um objeto json. Se eu tentar irá receber null
 
 routes(app)
-
-
-
-app.get("/livros/:id", (req, res) => {
-    let index = buscaLivro(req.params.id)
-    res.json(livros[index])
-})
-
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body)
-    res.status(201).send("Livro cadastrado")
-})
-
-app.put("/livros/:id", (req, res) => {
-    let index = buscaLivro(req.params.id)
-    livros[index].titulo = req.body.titulo
-    res.json(livros)
-})
-
-app.delete("/livros/:id", (req, res) => {
-    let {id} = req.params
-    let index = buscaLivro(id)
-    livros.splice(index, 1)
-    res.send(`Livro ${id} removido com sucesso`)
-})
-
-
-const buscaLivro = (id) => {
-    return livros.findIndex(livro => livro.id == id)//usar === não deu certo pois o parâmetro da função é do tipo any
-}
 
 export default app
