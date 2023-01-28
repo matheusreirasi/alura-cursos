7759a0595398d68aed219456769f15eb4960afea
matheusreirasi <matheus.reirasi@gmail.com>
2023-01-27 23:50:05 -0300

    rotas contendo os verbos HTTP e as funções criadas no controller são executadas aqui

---
+++ api-com-express-e-mongodb/src/routes/autoresRoutes.js
@@ -0,0 +1,13 @@
+import express from "express"
+import AutorController from "../controllers/autoresControllers.js"
+
+const router = express.Router()
+
+router.get("/autores", AutorController.listarAutores)
+router.get("/autores/:id", AutorController.listarAutorId)
+router.post("/autores", AutorController.cadastrarAutor)
+router.put("/autores/:id", AutorController.atualizarAutor)
+router.delete("/autores/:id", AutorController.removerAutor)
+
+
+export default router
