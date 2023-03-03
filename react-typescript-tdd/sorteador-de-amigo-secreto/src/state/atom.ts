2557f5cb77ed8f5f569a3ffb742d5fbd76f341d6
matheusreirasi <matheus.reirasi@gmail.com>
2023-03-02 21:04:57 -0300

    atom do resultado do amigo secreto

--- react-typescript-tdd/sorteador-de-amigo-secreto/src/state/atom.ts
+++ react-typescript-tdd/sorteador-de-amigo-secreto/src/state/atom.ts
@@ -10,4 +10,9 @@ export const erroState = atom<string>({
     default: ''
 })
 
+export const resultadoAmigoSecreto = atom<Map<string, string>>({
+    key: 'resultadoAmigoSecreto',
+    default: new Map()
+})
+
 //esse atom é uma pequena porção do estado que posteriormente eu posso passar para uma função que lida com estados como useState(listaParticipantesState)
