3e39537fbd47bfa42514c3270baecd26b90612d6
matheusreirasi <matheus.reirasi@gmail.com>
2023-03-02 21:08:05 -0300

    hook do sorteador e resultado do sorteio

---
+++ react-typescript-tdd/sorteador-de-amigo-secreto/src/state/hooks/useSorteador.ts
@@ -0,0 +1,15 @@
+import { useListaParticipantes } from "./useListaParticipantes"
+import { useSetRecoilState } from "recoil"
+import { resultadoAmigoSecreto } from "../atom"
+import { realizarSorteio } from "../helpers/realizarSorteio"
+
+export const useSorteador = () => {
+
+    const participantes = useListaParticipantes()
+    const setResultado = useSetRecoilState(resultadoAmigoSecreto)
+
+    return () => {
+        const resultado = realizarSorteio(participantes)
+        setResultado(resultado)
+    }
+}
